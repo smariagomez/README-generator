5d5a7268d060b7527efad4a7552b040d032672dc
Stephanie Gomez <stephaniemariagomez@gmail.com>
2022-07-18 22:21:19 -0700

    added walkthrough video link

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./generateMarkdown')
